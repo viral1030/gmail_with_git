dfcc633abd0618a85df5a69c2066a1832e164209
Viral.patel <viral.patel@kiwiqa.com>
2015-10-28 11:23:34 +0530

    QA-1 Status : Completed , Message : Added new test gmail

--- gmail/src/test/java/com/gmail/verifications/Index.java
+++ gmail/src/test/java/com/gmail/verifications/Index.java
@@ -45,6 +45,41 @@ public class Index extends SeleniumInit
 		}
 	}
 	
+	
+	
+	
+	@Test
+	public void Gmail_login_viral() //facebook login
+	{
+        /* log here ..*/   
+		common.log("<br></br>Step 1:open Google.com");
+		common.log("<br></br>Step 2:clcik on Gmail link '");
+		common.log("<br></br>Step 3:Enter the valid detail into form field" +
+				"   <br></br> Email: krazzyvir@gmail.com" +
+				"   <br></br> Password : 8518krazzyvir1933");
+		common.log("<br></br> Step 4 : click on compose Button");
+		common.log("<br></br> Step 5 : Enter The Detail for sending mail" +
+				"   <br>TO : viral103patel@gmail.com " +
+				"   </br>subject: automation " +
+				"   </br>Body :  this mail is send by automation script");
+		common.log("<br></br>Step 6: Click on send Button");
+		
+		 DashboardPage db = indexpage.login();
+		/*comment removed*/
+		if (db.verify_gmail_send())
+		{
+			Assert.assertTrue(true);
+			
+
+		} else {
+			Assert.assertTrue(false);
+			log("<br></br>FAIL");
+
+		}
+	}
+	
+	
+	
 		
 		
 	}
