e85ce23c1c8b23ce061830482fdd0bd0c0087084
Tarpan Patel <tarpan.patel@kiwiqa.com>
2015-10-28 14:50:39 +0530

    QA 28

--- gmail/src/test/java/com/gmail/verifications/Index.java
+++ gmail/src/test/java/com/gmail/verifications/Index.java
@@ -63,7 +63,7 @@ public class Index extends SeleniumInit
 		common.log("<br></br>Step 2:clcik on Gmail link '");
 		common.log("<br></br>Step 3:Enter the valid detail into form field" +
 				"   <br></br> Email: krazzyvir@gmail.com" +
-				"   <br></br> Password : rameshsuresh");
+				"   <br></br> Password : --tarpan password--");
 
 		common.log("<br></br> Step 4 : click on compose Button");
 		common.log("<br></br> Step 5 : Enter The Detail for sending mail" +
