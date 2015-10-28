e89492a479500b5794e74c4060eaa820adea1d3f
Tarpan Patel <tarpan.patel@kiwiqa.com>
2015-10-28 15:27:12 +0530

    QA 45

--- gmail/src/test/java/com/gmail/verifications/Index.java
+++ gmail/src/test/java/com/gmail/verifications/Index.java
@@ -7,24 +7,8 @@ import com.gmail.Init.Common;
 import com.gmail.Init.SeleniumInit;
 import com.gmail.pages.DashboardPage;
 
-public class Index extends SeleniumInit {
-	int rudresh = 1;
-	int c = 556;
-	public int viral_rudresh() {
-		return rudresh-rudresh;
-	}
-
-public int rt()
 {
-	
-	System.out.println("=======");
-	System.out.println("*****");
-	System.out.println("++++++");
-	return 7;
-	
-	
-	
-	
-	
-}
+
+
+
 }
